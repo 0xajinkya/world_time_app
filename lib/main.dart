e7010661cf0a6f22c8b0c40dbe99df42ad3aa31c
Ajinkya <kahaneajinkya051@gmail.com>
2022-12-01 22:49:00 +0530

    API Requests

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:world_time_app/pages/loading.dart';
 import 'package:world_time_app/pages/choose_location.dart';
 
 void main() => runApp(MaterialApp(
-  initialRoute: '/home',
+  initialRoute: '/',
   routes: {
     '/home': (context) => Home(),
     '/': (context) => Loading(),
--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,8 @@
-import 'package:flutter/material.dart';
+import 'dart:html';
 
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -9,6 +12,41 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  final String baseUrl = 'https://worldtimeapi.org/api/timezone/';
+  var client = Client();
+
+  // Future<dynamic> getData(String baseUrl, String api) async {
+  //   var url = Uri.parse(baseUrl + api);
+  //
+  //   Response response = await client.get(url);
+  //   Map data = jsonDecode(response.body);
+  //   print(data['title']);
+  // }
+
+  Future <dynamic> getTime(String api) async {
+    var url = Uri.parse(baseUrl + api);
+    Response response = await client.get(url);
+    Map data = jsonDecode(response.body);
+    // print(data);
+
+    //Getting The Properties From Data
+    String dateTime = data['datetime'];
+    String offset = data['utc_offset'].substring(1, 3);
+    // print(dateTime);
+    // print(offset);
+
+    //Create A DateTime Object
+    DateTime now = DateTime.parse(dateTime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
+  }
+
+  @override
+  void initState() {
+    super.initState;
+    getTime('Europe/London');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
